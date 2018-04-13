94bca7566d1974630bfc9fd83149710eca736b31
rashintha <rashinthamaduneth@gmail.com>
2018-04-13 12:11:03 +0530

    Web interface building..

--- Cluster Server/data.py
+++ Cluster Server/data.py
@@ -1,7 +1,13 @@
 import psutil
+import json
 
 def get_used_mem():
     return psutil.virtual_memory().used
 
+def get_total_mem():
+    return psutil.virtual_memory().total
+
 def get_data():
-    return str("{0:.2f}".format(get_used_mem() / (1024 * 1024 * 1024))) + " GB"
+    return json.dumps("RAM_USED": str("{0:.2f}".format(get_used_mem() / (1024 * 1024 * 1024))),
+        "RAM_TOTAL": str("{0:.2f}".format(get_total_mem() / (1024 * 1024 * 1024)))
+        )
